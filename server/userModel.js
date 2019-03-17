450d2c7e7accd95fe528478bf9846a98c71c948a
abrahamodukoya <abrahamodukoya@yahoo.ie>
2019-03-17 17:05:50 +0000

    reorganised description of schemas; patch updates seem to persist; more testing needed

--- server/userModel.js
+++ server/userModel.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose');
+const activitySchema = mongoose.Schema({
+    _id : false,
+    type: String,
+    label: String,
+    dayName: String,
+    date: String,
+    duration: Number
+});
+const daySchema = mongoose.Schema({
+    _id : false,
+    index: false,
+    activity: [activitySchema]
+});
 const userSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
@@ -6,23 +19,8 @@ const userSchema = mongoose.Schema({
     username: String,
     week: {
         index: false,
-        day: [
-            {
-                _id : false,
-                index: false,
-                activity: [
-                    {
-                        _id : false,
-                        type: String,
-                        label: String,
-                        dayName: String,
-                        date: String,
-                        duration: Number
-                    }
-                ]
-            },
-        ],
-    },   
+        day: [daySchema],
+    },
 });
 
 module.exports = mongoose.model('User', userSchema);
