fb49795d95f7220986d7fbdbbceedc14801975be
kamilprz <36544756+kamilprz@users.noreply.github.com>
2019-03-20 15:18:24 +0000

    made changes to user model
    moved "data" to day object instead of activity
    added (temporary?) password property to user
    removed "dayName" from activity

--- server/userModel.js
+++ server/userModel.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 const activitySchema = mongoose.Schema({
     _id : false,
-    type: String,
+    type: {type: String, required : true},
     label: String,
-    dayName: String,
-    date: String,
-    duration: Number
+    duration: {type: String, required : true}
 });
 const daySchema = mongoose.Schema({
     _id : false,
     index: false,
+    date: String,
     activity: [activitySchema]
 });
 const userSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    username: String,
-    week: {
+    username: {type: String, required : true},
+    password: {type: String, required : true},
+    data: {
         index: false,
         day: [daySchema],
     },
