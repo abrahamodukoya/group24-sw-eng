aef88b27af6cc1966ab91e12b2f6466e42d8a9c0
kamilprz <36544756+kamilprz@users.noreply.github.com>
2019-03-24 17:58:25 +0000

    found and fixed a deprecation warning

--- server/userRoutes.js
+++ server/userRoutes.js
@@ -268,7 +268,7 @@ router.patch('/users/:id', checkAuth, (req, res, next)=> {
 //http://3.92.227.189:80/api/users/_id
 router.delete('/users/:id', checkAuth, (req, res, next) => {
     const id = req.params.id;
-    User.remove({_id: id})
+    User.deleteOne({_id: id})
     .exec()
     .then(result => {
         res.status(200).json({
